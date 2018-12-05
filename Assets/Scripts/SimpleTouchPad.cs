85918ffefb6615a37df044329ecf7286c7cd0371
RezaAzadeh <azadeh.reza@gmail.com>
2018-12-05 19:46:27 +0330

    TouchPad Script

---
+++ Assets/Scripts/SimpleTouchPad.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SimpleTouchPad : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler {
+
+    private Vector2 origin;
+    private Vector2 direction;
+
+    void Awake()
+    {
+        direction = Vector2.zero;
+    }
+	public void OnPointerDown (PointerEventData data)
+    {
+        origin = data.position;
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        Vector2 currentPosition = data.position;
+        Vector2 directionRaw = currentPosition - origin;
+        direction = directionRaw.normalized;
+    }
+
+    public void OnPointerUp(PointerEventData data)
+    {
+        direction = Vector2.zero;
+    }
+
+    public Vector2 GetDirection()
+    {
+        return direction;
+    }
+}
